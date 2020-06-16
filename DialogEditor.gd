f39f2bb474d02c43655e82dc77eda61a99ac7995
fossegutten <kjetil.fosse@outlook.com>
2020-06-16 18:26:59 +0200

    as

--- DialogEditor.gd
+++ DialogEditor.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var graph_edit : GraphEdit = $VBox/HBox2/GraphEdit
 
+onready var dw_node : PackedScene = preload("res://nodes/DWNode.tscn")
 
 func assign_node_id(node : DWNode) -> void:
 	
@@ -25,15 +26,47 @@ func get_dw_nodes() -> Array:
 	return nodes
 
 func _on_AddNodeButton_pressed():
-	var new : DWNode = DWNode.new()
+	var new : DWNode = dw_node.instance()
 	new.connect("close_request", self, "_on_DWNode_close_request", [new])
 	new.offset = get_dw_nodes().size() * Vector2(60, 40)
 	graph_edit.add_child(new)
 	assign_node_id(new)
-	new.name = "Node %d" % [new.get_node_id()]
-	new.title = "Node %d" % [new.get_node_id()]
+	new.name = "Node name %d" % [new.get_node_id()]
+	new.title = "Node title %d" % [new.get_node_id()]
 
 
 func _on_DWNode_close_request(requester : GraphNode) -> void:
+	
+	# remove all connections
+	for i in graph_edit.get_connection_list():
+		if i.from == requester.name or i.to == requester.name:
+			graph_edit.disconnect_node(i.from, i.from_port, i.to, i.to_port)
+	
 	requester.queue_free()
-	pass
+
+
+func _on_GraphEdit_connection_request(from, from_slot, to, to_slot):
+	if from == to:
+		return
+	
+	# max one target connection
+	for i in graph_edit.get_connection_list():
+		if i.from == from:
+			graph_edit.disconnect_node(i.from, i.from_port, i.to, i.to_port)
+	
+	print(from, from_slot, to, to_slot)
+	var err := graph_edit.connect_node(from, from_slot, to, to_slot)
+	if err != OK:
+		printerr("Error connecting node: %s" % err)
+
+
+#func _on_GraphEdit_connection_from_empty(to, to_slot, release_position):
+#	for i in graph_edit.get_connection_list():
+#		if i.to == to and i.to_port == to_slot:
+#			graph_edit.disconnect_node(i.from, i.from_port, i.to, i.to_port)
+
+
+func _on_GraphEdit_connection_to_empty(from, from_slot, release_position):
+	for i in graph_edit.get_connection_list():
+		if i.from == from and i.from_port == from_slot:
+			graph_edit.disconnect_node(i.from, i.from_port, i.to, i.to_port)
--- nodes/DWNode.gd
+++ nodes/DWNode.gd
@@ -17,11 +17,22 @@ func get_node_id() -> int:
 func _ready():
 	show_close = true
 	resizable = true
-	rect_size = Vector2(200, 100)
+#	rect_size = Vector2(200, 100)
 	set_slot(0, true, 0, Color.white, true, 0, Color.white)
-	add_child(Label.new())
-	set_slot(1, false, 1, Color.white, true, 1, Color.white)
-	add_child(Label.new())
+	set_slot(1, false, 0, Color.white, true, 0, Color.white)
+	set_slot(2, false, 0, Color.white, true, 0, Color.white)
+	var label = Label.new()
+	var label2 = Label.new()
+	label2.align = Label.ALIGN_RIGHT
+	var label3 = Label.new()
+	label3.align = Label.ALIGN_RIGHT
+	label.text = "test"
+	label2.text = "test2"
+	label3.text = "test3"
+#	add_child(label)
+#	set_slot(1, false, 1, Color.white, true, 1, Color.white)
+#	add_child(label2)
+#	add_child(label3)
 #	set_conn
 	print(get_connection_output_count())
 #	print(get_connection_input_count())
